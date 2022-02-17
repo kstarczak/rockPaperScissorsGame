d1bc565fd244242793f4bbf0d1c5fb98c96583b6
Konrad Starczak <k.starczak@gmail.com>
2022-02-17 17:38:49 -0500

    add laptop changes: game rules page and working buttons

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
 const plays = ["rock", "paper", "scissors", "lizard", "spock"];;
-let playerWins = 0;
-let computerWins = 0;
+const winnerDeclaration = document.querySelector('.winnerDeclaration');
+const score = document.querySelector('.score');
+const playButtons = document.querySelectorAll('.playButton');
+
+
+
+const testButton = document.querySelector('.testButton');
 
 function computerPlay() {
     return plays[Math.floor(plays.length * Math.random())];
     }
 
-function playerPlay() {
-    return prompt("Choose rock, paper, scissors, lizard, or Spock").toLowerCase();
-    // delete code above and let playerPlay equal be reassigned to button with event listener and start game
-}
-
-
-function playRound(playerSelection,computerSelection) {
+//revise function below to return the winner
+function playRound(e) {
+    const computerSelection = computerPlay();
+    const playerSelection = this.id;
+    console.log('the computer picked: ' + computerSelection);
+    console.log('you picked: ' + playerSelection);
     if (playerSelection === 'marmot') {
-        alert("The Marmot wins the fox's heart!");
+        winnerDeclaration.textContent ='The Marmot wins the fox\'s heart!';
     } else if (playerSelection === computerSelection) {
-        alert('You tied! Try again!');
+        winnerDeclaration.textContent = 'You tied! Try again!';
     } else if ((playerSelection === 'spock' && (computerSelection === 'scissors' ||computerSelection === 'rock')
     || (playerSelection === 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard'))
     || (playerSelection === 'paper' && (computerSelection === 'rock' || computerSelection === 'spock'))
     || (playerSelection === 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard'))
     || (playerSelection === 'lizard' && (computerSelection === 'paper' || computerSelection ===  'spock')))) {
-        alert(`${playerSelection} beats ${computerSelection}. You won!`); 
-        playerWins += 1;
+        winnerDeclaration.textContent = `${playerSelection} beats ${computerSelection}. You won!`; 
+        //playerWins += 1;
+        //score.textContent = `Youre score: ${playerWins}        Computer score: ${computerWins}`;
     } else {
-        alert(`${playerSelection} loses to ${computerSelection}. You lost!`);
-        computerWins += 1;
+        winnerDeclaration.textContent = `${playerSelection} loses to ${computerSelection}. You lost!`;
+        //computerWins += 1;
+        //score.textContent = `Youre score: ${playerWins}        Computer score: ${computerWins}`;
     }
 }
 
-
-function game() {
-    playRound (playerPlay(),computerPlay());
-    playRound (playerPlay(),computerPlay());
-    playRound (playerPlay(),computerPlay());
-    playRound (playerPlay(),computerPlay());
-    playRound (playerPlay(),computerPlay());
-    if (playerWins === computerWins) {
-        alert('You tied 5 times! What are the chances!?');
-    } else if (playerWins > computerWins) {
-        alert ('You beat the computer!');
+// execute playGame when page when clicks play game and play again
+function playGame(e) {
+    let playerWins = 0;
+    let computerWins = 0; 
+    playRound('rock');
+    // some counter function
+    playRound('scissors');
+    // some counter function
+    if (playerWins > computerWins) {
+        winnerDeclaration.textContent="You won. Congratulations on winning a game the requires absolutely no critical thinking skills. You must be so proud of yourself"; 
     } else {
-        alert ('You lost to the computer.')
+        winnerDeclaration.textContent="You're a loser and your parents don't even love you.";
     }
 };
 
-game();
+
+playButtons.forEach(button => button.addEventListener('click' , playRound));
+
+
+//addEventListener('click', playRound(button identifier))
+
